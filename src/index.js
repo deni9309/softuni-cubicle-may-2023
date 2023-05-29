22c5596cee3bf6c88f6d961835c6fb765f23b208
Denitsa Slavkova <deni9309@gmail.com>
2023-05-29 18:38:40 +0300

    setup mongoose

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const dbConnect = require('./config/dbConfig');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const routes = require('./routes');
@@ -10,6 +11,10 @@ const PORT = 5000;
 
 expressConfig(app);
 handlebarsConfig(app);
+
+dbConnect()
+    .then(() => console.log('DB connected successfully'))
+    .catch(err => console.log('DB error: ', err));
 
 app.use(routes);
 
